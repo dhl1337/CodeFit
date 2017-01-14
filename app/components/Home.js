2f160c99f0fe28c67feae703f1b6e242920401f6
Dan Le <dhl1337@gmail.com>
2017-01-14 00:52:40 -0700

    add text editor on home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import CodeMirror from 'react-codemirror'
 
 class Home extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            code: "// Code",
+        }
+    }
+
+    getInitialState() {
+        return {
+            code: "// Code",
+        };
+    }
+
+    updateCode(newCode) {
+        this.setState({
+            code: newCode,
+        });
+    }
+
     render() {
+        var options = {
+            lineNumbers: true
+        };
         return (
-            <div>
-                Home
+            <div className="ui container">
+                <CodeMirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options}/>
+                <div className="ui button">Submit</div>
             </div>
         )
+
+
     }
 }
 
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Home from './Home';
 
 class Main extends React.Component {
     render() {
         return (
             <div>
-                <h1>Main</h1>
+                <Home/>
             </div>
         )
     }

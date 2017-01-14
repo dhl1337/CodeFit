eeaecd425e3071b5c099a0524188139b2a547ff1
Dan Le <dhl1337@gmail.com>
2017-01-14 00:09:02 -0700

    route is finally working

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,7 +4,7 @@ class Main extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <h1>Main</h1>
             </div>
         )
     }

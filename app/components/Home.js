fe95396a7025d1fd96f312e891ced46f41822d70
Dan Le <dhl1337@gmail.com>
2017-01-14 16:37:59 -0700

    have layout of one exercise complete

--- app/components/Home.js
+++ app/components/Home.js
@@ -7,13 +7,19 @@ class Home extends React.Component {
         super();
 
         this.state = {
-            code: "// Code",
+            code: `(function () {
+     
+            
+            
+    return studentArray
+})()`,
+            colorLabel: red
         }
     }
 
     getInitialState() {
         return {
-            code: "// Code",
+            code: "",
         };
     }
 
@@ -24,7 +30,10 @@ class Home extends React.Component {
     }
 
     handleSubmit() {
-        console.log(this.state.code);
+
+        var a = eval(this.state.code);
+        console.log(a);
+
     }
 
     render() {
@@ -34,12 +43,48 @@ class Home extends React.Component {
         };
         return (
             <div className="ui container">
-                <CodeMirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options}/>
-                <div className="ui button" onClick={() => this.handleSubmit()}>Submit</div>
+                <div className="ui stackable grid">
+                    <div className="five wide column">
+                        <header>
+                            <h1 className="ui header">Array</h1>
+                            <div className="ui divider"></div>
+                            <div className="ui circular labels">
+                                <a className="ui label">
+                                    1
+                                </a>
+                                <a className="ui label">
+                                    2
+                                </a>
+                                <a className="ui label">
+                                    3
+                                </a>
+                                <a className="ui label">
+                                    4
+                                </a>
+                                <a className="ui label">
+                                    5
+                                </a>
+                            </div>
+                            <div className="ui segment">
+                                <div className="description">
+                                    <p>Declare an array named studentArray and add two string to it. One is your full name
+                                        and the second is github username</p>
+                                </div>
+                            </div>
+                        </header>
+                    </div>
+                    <div className="eleven wide column">
+                        <div className="ui tabular menu">
+                            <a className="item active">
+                                Application.js
+                            </a>
+                        </div>
+                        <CodeMirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options}/>
+                        <div className="ui code submit button" onClick={() => this.handleSubmit()}>Submit</div>
+                    </div>
+                </div>
             </div>
         )
-
-
     }
 }
 
